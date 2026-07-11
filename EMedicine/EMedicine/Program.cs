9121f79cf71e3a3948fd0726acec83b79b09b1e3
prakashsanjay230-ux <prakashsanjay230@gmail.com>
2026-07-11 22:56:58 +0530

    Enable Swagger in production

--- EMedicine/EMedicine/Program.cs
+++ EMedicine/EMedicine/Program.cs
@@ -99,11 +99,8 @@ builder.Services.AddAuthorization();
 var app = builder.Build();
 
 // ✅ Swagger
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(); 
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
