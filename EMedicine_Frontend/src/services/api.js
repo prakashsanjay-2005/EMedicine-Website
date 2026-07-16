76a31029339b28edabba8c1e1b523ec11a3a6b6b
prakashsanjay230-ux <prakashsanjay230@gmail.com>
2026-07-16 15:32:44 +0530

    Update frontend for Render backend

--- EMedicine_Frontend/src/services/api.js
+++ EMedicine_Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://localhost:7178/api",
+  baseURL: "https://emedicine-website.onrender.com/api",
 });
 
 export default api;
